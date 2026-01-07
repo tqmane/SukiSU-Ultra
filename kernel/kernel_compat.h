3c1b3761ac5ca3eeba7a95a706e54d98d567c2da
tqmane <nomu78@iwink.jp>
2026-01-07 22:59:52 +0900

    compat: add 5.4 fallbacks

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -2,6 +2,7 @@
 #define __KSU_H_KERNEL_COMPAT
 
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 #include <linux/version.h>
 
 /*
@@ -20,5 +21,24 @@ static long ksu_copy_from_user_retry(void *to, const void __user *from,
     // we faulted! fallback to slow path
     return copy_from_user(to, from, count);
 }
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
+static inline long copy_from_user_nofault(void *to, const void __user *from,
+					  unsigned long n)
+{
+	/*
+	 * copy_from_user returns number of uncopied bytes; convert to
+	 * 0/-EFAULT style used by newer nofault helper.
+	 */
+	return copy_from_user(to, from, n) ? -EFAULT : 0;
+}
+
+static inline long strncpy_from_user_nofault(char *dst,
+					     const char __user *src,
+					     long count)
+{
+	return strncpy_from_user(dst, src, count);
+}
+#endif
 
 #endif
